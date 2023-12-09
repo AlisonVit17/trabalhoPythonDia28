037412042ee06ca3f29af45e1b3c9e9d5a84d852
Guilherme Gomes <guilherme.silva@ufpi.edu.br>
2023-12-09 15:49:43 -0500

    code update

--- ClienteTrabalho1.py
+++ ClienteTrabalho1.py
@@ -1,4 +1,5 @@
 import socket
+
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QStackedWidget
 from PyQt5.uic import loadUi
 from qdarkstyle import load_stylesheet_pyqt5
@@ -37,11 +38,18 @@ class LoginPage(QMainWindow):
         self.mainPage.createAccount_button.clicked.connect(self.mostrar_cadastro)
         self.cadastroPage.voltar_button.clicked.connect(self.voltar_main_page)
         self.mainPage.login_button.clicked.connect(self.callback_login)
+        self.cadastroPage.cadastrar_button.clicked.connect(self.callback_cadastro)
         self.mainPage.exit_button.clicked.connect(self.callback_exit)
         self.mainPage.about_us.clicked.connect(self.about_uss)
         self.tela_02.logout_btn.clicked.connect(self.confirmarSaida)
+        self.tela_02.search_btn.clicked.connect(self.buscar_noticiasTela)
+        self.tela_02.envioProgramado.clicked.connect(self.confirmarSaida)
+        self.tela_02.envioProgramado.clicked.connect(self.programarEnvioTela)
+        self.programarEnvio.voltarMostrarTela.clicked.connect(self.confirmarSaida)
+        self.programarEnvio.programar.clicked.connect(self.programaEnvio)
+        self.programarEnvio.testarEvios.clicked.connect(self.programaEnvio)
         self.about_us.back_butt.clicked.connect(self.voltar_main_page)
-        self.confirmaSaida.logout.clicked.connect(self.voltar_main_page) 
+        self.confirmaSaida.logout.clicked.connect(self.voltar_main_page)
         self.confirmaSaida.cancel.clicked.connect(self.telaDeBusca)
 
         self.mainPage.email_or_phone.setPlaceholderText('Email')
@@ -64,7 +72,6 @@ class LoginPage(QMainWindow):
         self.stacked_widget.setCurrentIndex(1)
 
     def voltar_main_page(self):
-
         opcao = '0'
         self.client_socket.send(opcao.encode())
         self.stacked_widget.setCurrentIndex(0)
@@ -90,19 +97,160 @@ class LoginPage(QMainWindow):
         email = self.mainPage.email_or_phone.text()
         senha = self.mainPage.password.text()
         if email == '' or senha == '':
-            email = 'a'
-            senha = 'a'
+            QMessageBox.warning(self, 'Erro no login', 'Email e/ou senha incorretos')
+            return
         string = email + ',' + senha
+        print(string)
         self.client_socket.send(string.encode())
-        certo = self.client_socket.recv(1024).decode()
 
-        if certo == '1':
+        retorno = self.client_socket.recv(1024).decode()
+
+        if retorno == '1':
 
             self.stacked_widget.setCurrentIndex(2)
             QMessageBox.warning(self, 'Login', 'Login realizado com sucesso')
         else:
 
             QMessageBox.warning(self, 'Erro no login', 'Email e/ou senha incorretos')
+
+    def callback_cadastro(self):
+        opcao = '2'
+        self.client_socket.send(opcao.encode())
+
+        first_name = self.cadastroPage.first_name.text()
+        second_name = self.cadastroPage.second_name.text()
+        email_or_phone = self.cadastroPage.email_or_phone.text()
+        password = self.cadastroPage.password.text()
+        confirm_password = self.cadastroPage.confirm_password.text()
+        dia = int(self.cadastroPage.dia.currentText())
+        mes = int(self.cadastroPage.mes.currentText())
+        ano = int(self.cadastroPage.ano.currentText())
+        data_nascimento = str(dia) + '/' + str(mes) + '/' + str(ano)
+
+
+        string = f"{first_name},{second_name},{email_or_phone},{password},{data_nascimento}"
+
+        if first_name == '' or second_name == '' or email_or_phone == '' or password == '' or confirm_password == '':
+            QMessageBox.warning(self, 'Erro no cadastro', 'Preencha todos os campos')
+            return
+        if password != confirm_password:
+            QMessageBox.warning(self, 'Erro no cadastro', 'Senhas não conferem')
+            return
+
+        self.client_socket.send(string.encode())
+
+        try:
+            retorno = self.client_socket.recv(1024).decode()
+        except Exception as e:
+            print(f"Error during login: {e}")
+            QMessageBox.warning(self, "Erro no login", f"Ocorreu um erro: {e}")
+            return
+
+        if retorno == '1':
+            self.stacked_widget.setCurrentIndex(0)
+            QMessageBox.warning(self, 'Cadastro', 'Cadastro realizado com sucesso')
+        elif retorno == '-2':
+            QMessageBox.warning(self, 'Erro no cadastro', 'Email já existe')
+        else:
+            QMessageBox.warning(self, 'Erro no cadastro', 'Ocorreu um erro')
+
+        self.cadastroPage.first_name.clear()
+        self.cadastroPage.second_name.clear()
+        self.cadastroPage.email_or_phone.clear()
+        self.cadastroPage.password.clear()
+        self.cadastroPage.confirm_password.clear()
+        self.cadastroPage.dia.setCurrentIndex(0)
+        self.cadastroPage.mes.setCurrentIndex(0)
+        self.cadastroPage.ano.setCurrentIndex(0)
+
+
+    def buscar_noticiasTela(self):
+        opcao = '3'
+        self.client_socket.send(opcao.encode())
+
+        keyword = self.tela_02.key_word.text()
+        qntd_tela = int(self.tela_02.qntd_tela.currentText())
+        section = self.tela_02.filter_tela.currentText().lower()
+
+        if keyword == '':
+            QMessageBox.warning(self, 'Erro na busca', 'Digite uma palavra-chave')
+            return
+        if qntd_tela == '':
+            QMessageBox.warning(self, 'Erro na busca', 'Digite uma quantidade de notícias')
+            return
+        if section == '':
+            QMessageBox.warning(self, 'Erro na busca', 'Selecione uma seção')
+            return
+
+
+        string = f"{keyword},{qntd_tela},{section}"
+        self.client_socket.send(string.encode())
+
+
+        try:
+            retorno = self.client_socket.recv(1024).decode()
+        except Exception as e:
+            print(f"Error during login: {e}")
+            QMessageBox.warning(self, "Erro no login", f"Ocorreu um erro: {e}")
+            return
+
+        if retorno == '1':
+            self.stacked_widget.setCurrentIndex(2)
+            QMessageBox.warning(self, 'Busca', 'Busca realizada com sucesso')
+        elif retorno == '-1':
+            QMessageBox.warning(self, 'Ocorreu um erro na busca')
+        elif retorno == '-2':
+            QMessageBox.warning(self, 'Erro na busca', 'Ocorreu um erro na palavra chave')
+        self.tela_02.key_word.clear()
+        self.tela_02.qntd_tela.clear()
+
+
+    def programaEnvio(self):
+        opcao = '4'
+        self.client_socket.send(opcao.encode())
+
+        quantidade = self.programarEnvio.qntdTela.currentText()
+        lingua = self.programarEnvio.lingua_tela.currentText()
+        frenquencia = self.programarEnvio.frequenciaEmails.currentText()
+
+        if quantidade == '':
+            QMessageBox.warning(self, 'Erro na busca', 'Digite uma quantidade de notícias')
+            return
+        if lingua == '':
+            QMessageBox.warning(self, 'Erro na busca', 'Selecione uma lingua')
+            return
+        if frenquencia == '':
+            QMessageBox.warning(self, 'Erro na busca', 'Selecione uma frequência')
+            return
+
+        string = f"{quantidade},{lingua},{frenquencia}"
+        self.client_socket.send(string.encode())
+
+        try:
+            retorno = self.client_socket.recv(1024).decode()
+        except Exception as e:
+            print(f"Error during login: {e}")
+            QMessageBox.warning(self, "Erro no login", f"Ocorreu um erro: {e}")
+            return
+
+        if retorno == '1':
+            self.stacked_widget.setCurrentIndex(5)
+            QMessageBox.warning(self, 'Busca', 'Busca realizada com sucesso')
+        elif retorno == '-1':
+            QMessageBox.warning(self, 'Ocorreu um erro na busca')
+        elif retorno == '-2':
+            QMessageBox.warning(self, 'Erro na busca', 'Ocorreu um erro na palavra chave')
+
+        self.programarEnvio.key_word.clear()
+        self.programarEnvio.qntd_tela.clear()
+
+
+
+    def programarEnvioTela(self):
+        opcao = '4'
+        self.client_socket.send(opcao.encode())
+        self.stacked_widget.setCurrentIndex(5)
+
 
     def callback_exit(self):
         opcao = '-1'
@@ -121,15 +269,14 @@ class LoginPage(QMainWindow):
         self.stacked_widget.setCurrentIndex(4)
 
     def telaDeBusca(self):
-
-        opcao = '0'
+        opcao = '3'
         self.client_socket.send(opcao.encode())
         self.stacked_widget.setCurrentIndex(2)
 
 
-ip = '192.168.18.46'
 
-port = 9008
+ip = '192.168.18.46'
+port = 9002
 
 addr = (ip, port)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- ServidorTrabalho1.py
+++ ServidorTrabalho1.py
@@ -1,6 +1,10 @@
 import socket
 import threading
 import mysql.connector
+import hashlib
+import datetime
+import requests
+from bs4 import BeautifulSoup
 
 class ClientThread(threading.Thread):
 
@@ -18,40 +22,176 @@ class ClientThread(threading.Thread):
 
                 opcao = self.conexao.recv(1024)
                 if opcao.decode() == '1':
-                    
-                    email = self.conexao.recv(1024).decode()
-                    senha = self.conexao.recv(1024).decode()
+                    recebido = self.conexao.recv(1024).decode().split(',')
+                    email = recebido[0]
+                    senha = recebido[1]
                     enviar = self.verificaLogin(email, senha)
                     self.conexao.send(enviar.encode())
-                if opcao.decode() == '-1':
+                elif opcao.decode() == '2':
+                    data = self.conexao.recv(1024).decode().split(',')
+
+                    if len(data) != 5:
+                        self.conexao.send('-1'.encode())
+                        return
+
+                    first_name = data[0]
+                    second_name = data[1]
+                    email = data[2]
+                    password = data[3]
+                    data_nascimento = data[4]
+                    self.cadastrar(first_name, second_name, email, password, data_nascimento)
+                elif opcao.decode() == '3':
+                    data = self.conexao.recv(1024).decode().split(',')
+                    keyword = data[0]
+                    qntd_tela = int(data[1])
+                    section = data[2]
+                    self.buscarNoticias(keyword, qntd_tela, section)
+                elif opcao.decode() == '4':
+                    data = self.conexao.recv(1024).decode().split(',')
+                    keyword = data[0]
+                    qntd_tela = int(data[1])
+                    section = data[2]
+                    self.programarNoticias(keyword, qntd_tela, section)
+                elif opcao.decode() == '-1':
 
                     break
                 else:
-                    
+
                     pass
         except:
-            
+
             pass
         print('Disconnected')
         self.conexao.close()
-        self.startEnd.set()        
-    def verificaLogin(self, email, senha):
-        retorno = '0'
+        self.startEnd.set()
 
+    def verificaLogin(self, email, senha):
+
+        senha_hash = hashlib.sha256(senha.encode()).hexdigest()
         conexao = self.conectar_banco()
         cursor = conexao.cursor()
 
-        query = "SELECT * FROM usuarios WHERE email = %s AND senha = %s;"
-        cursor.execute(query, (email, senha))
+        query = "SELECT * FROM usuarios WHERE email_ou_telefone = %s AND senha = %s;"
+        cursor.execute(query, (email, senha_hash))
         usuario = cursor.fetchone()
 
         cursor.close()
         conexao.close()
 
         if usuario:
-            retorno = '1'
+            if usuario[4] == senha_hash:
+                retorno = '1'
+            else:
+                retorno = '-2'
+        else:
+            retorno = '-1'
 
-        return retorno;
+        return retorno
+
+    def cadastrar(self, first_name, second_name, email, senha, data_nascimento):
+        conexao = self.conectar_banco()
+        cursor = conexao.cursor()
+
+        query = "SELECT * FROM usuarios WHERE email_ou_telefone = %s;"
+        cursor.execute(query, (email,))
+        usuario = cursor.fetchone()
+
+        if usuario:
+            self.conexao.send('-2'.encode())
+            cursor.close()
+            conexao.close()
+            return
+
+        senha_hash = hashlib.sha256(senha.encode()).hexdigest()
+
+        if conexao:
+            cursor = conexao.cursor()
+            data_nasci = datetime.datetime.strptime(data_nascimento, '%d/%m/%Y').date()
+
+            query = "INSERT INTO usuarios (primeiro_nome, segundo_nome, email_ou_telefone, senha, data_nascimento) VALUES (%s, %s, %s, %s, %s);"
+            cursor.execute(query, (first_name, second_name, email, senha_hash, data_nasci))
+            conexao.commit()
+            self.conexao.send('1'.encode())
+
+            cursor.close()
+            conexao.close()
+        else:
+            print("Erro ao conectar ao banco de dados")
+
+    def buscarNoticias(self, keyword, qntd_tela, section):
+
+
+        url = f'https://news.google.com/search?q={keyword}&hl=en-US&gl=US&ceid=US:en&section={section}'
+        print("enttrei na busca de noticias")
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            print("passei do codigo de 200")
+            soup = BeautifulSoup(response.content, 'html.parser')
+            articles = soup.find_all('h3', class_='ipQwMb')
+
+            matching_news = []
+
+            for i, article in enumerate(articles):
+                if i >= qntd_tela:
+                    break
+                title = article.text
+                link = 'https://news.google.com' + article.a['href']
+                print("cheguei antes do retorno")
+                if keyword.lower() in title.lower():
+                    matching_news.append({'title': title, 'link': link})
+                    return "1"
+
+                else:
+                    return "-2"
+        else:
+            return "-1"
+
+    def programarNoticias(self, keyword, qntd_tela, section):
+
+            url = f'https://news.google.com/search?q={keyword}&hl=en-US&gl=US&ceid=US:en&section={section}'
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                articles = soup.find_all('h3', class_='ipQwMb')
+
+                matching_news = []
+
+                for i, article in enumerate(articles):
+                    if i >= qntd_tela:
+                        break
+                    title = article.text
+                    link = 'https://news.google.com' + article.a['href']
+
+                    if keyword.lower() in title.lower():
+                        matching_news.append({'title': title, 'link': link})
+
+                if len(matching_news) > 0:
+                    conexao = self.conectar_banco()
+                    cursor = conexao.cursor()
+
+                    query = "SELECT * FROM usuarios;"
+                    cursor.execute(query)
+                    usuarios = cursor.fetchall()
+
+                    for usuario in usuarios:
+                        email = usuario[3]
+                        senha = usuario[4]
+                        self.enviarEmail(email, senha, matching_news)
+
+                    cursor.close()
+                    conexao.close()
+
+                else:
+                    print("Nenhuma notícia encontrada")
+            else:
+                print("Erro ao conectar ao Google News")
+
+    def enviarEmail(self, email, senha, noticias):
+        print("entrou no enviar email")
+        pass
+
     def conectar_banco(self):
 
         conexao = mysql.connector.connect(
@@ -61,11 +201,11 @@ class ClientThread(threading.Thread):
             database= "project_webScraping"
         )
         return conexao
-    
+
 
 host = ''
 
-port = 9008
+port = 9002
 
 addr = (host, port)
 
@@ -76,7 +216,7 @@ serv_socket.listen(30)
 
 
 while(True):
-    
+
     try:
         print('aguardando conexao...')
         con, cliente = serv_socket.accept()
